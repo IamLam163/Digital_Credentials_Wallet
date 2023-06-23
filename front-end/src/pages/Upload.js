e15da3542085b7a0c0f2f3a169ad20fc3c693d67
erickadikah <erickadikah2030@gmail.com>
2023-06-23 14:33:30 +0300

    last implementation to front-end

--- front-end/src/pages/Upload.js
+++ front-end/src/pages/Upload.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Upload.css';
+import { Button } from '@mantine/core';
+import { BiImageAdd } from 'react-icons/bi';
+import { HiOutlineCloudUpload } from 'react-icons/hi'
+import { useNavigate } from 'react-router-dom';
 
 const Upload = () => {
   const [name, setName] = useState('');
   const [image, setImage] = useState(null);
+  const navigate = useNavigate()
+  const [previewImage, setPreviewImage] = useState(null);
+
+
+  // useEffect(() => {
+  //   const redirectTimer = setTimeout(() => {
+  //     navigate('/files'); // Redirect to the login page after 5 seconds
+  //   }, 1000);
+
+  //   return () => clearTimeout(redirectTimer); // Cleanup the timer when the component unmounts
+  // }, [navigate]);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -30,6 +46,7 @@ const Upload = () => {
       });
 
       console.log(response.data);
+      navigate('/files')
       // Handle success or redirect to another page
     } catch (error) {
       console.log(error);
@@ -38,20 +55,42 @@ const Upload = () => {
   };
 
   return (
-    <div>
-      <h2>Upload CV</h2>
+    <div className='outer'>
+      <HiOutlineCloudUpload style={{ marginBottom: '30px', fontSize: '75px'}}/>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className='inner'>
           <label>Name:</label>
-          <input type="text" value={name} onChange={handleNameChange} />
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            className="name-input"
+            style={{ height: '40px', borderRadius: '10px', border: '2px solid #ccc', marginLeft: '20px' }}
+          />
         </div>
-        <div>
+        <div className='upper'>
           <label>Image:</label>
-          <input type="file" onChange={handleImageChange} />
+          <Button
+            variant='outline'
+            component="label"
+            style={{ marginLeft: '20px' }}
+            className="file-input-button"
+          >
+            <BiImageAdd></BiImageAdd> Choose File
+            <input type="file" style={{ display: 'none' }} onChange={handleImageChange} />
+          </Button>
+          {previewImage && (
+            <div className="image-preview">
+              <img src={previewImage} alt="Preview" style={{ maxWidth: '200px', marginTop: '10px' }} />
+            </div>
+          )}
         </div>
-        <button type="submit">Upload</button>
+        <Button variant='outline' type="submit" className="upload-button">
+          Upload
+        </Button>
       </form>
     </div>
   );
 };
+
 export default Upload;
