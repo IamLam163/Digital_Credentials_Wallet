1b6b529d8e20d5c5f68fc7fe1501fc04497e6623
IamLam163 <olamidealiyu@gmail.com>
2023-06-29 01:39:38 +0100

    changed folder functionality

--- front-end/src/pages/Folder.js
+++ front-end/src/pages/Folder.js
@@ -1,3 +1,61 @@
+import React from 'react'
+import axios from 'axios'
+import AddFolderButton from './AddFolderButton'
+import { useFolder } from '../components/hooks/useFolder'
+import { BsFolder } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+
+function Folder() {
+  const { folder } = useFolder('648faee62f6c7a4104ad5ea1');
+  const [folders, setFolders] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const state = useFolder()
+  console.log(state)
+  //fetch all folders
+  useEffect(() => {
+    (
+      async () => {
+        try {
+          const { data } = await axios.get('/profile');
+          setCurrentUser(data);
+          let res = await axios.get(`/folder/user/${data?.id}`);
+          setFolders(res.data.folder);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    )()
+  }, [])
+
+
+  return (
+    <>
+      <div className='middle'>
+        <div className='buttonF'>
+          <AddFolderButton currentFolder={folder} />
+
+          <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', marginTop: "50px", gap: "20px" }}>
+            {
+              folders && folders.map(childFolder => (
+                <div key={childFolder.id} style={{ alignItems: 'center' }}>
+                  <BsFolder style={{ fontSize: '40', color: "white" }} />
+                  <p style={{ marginTop: "10px", color: "white" }}>
+                    {childFolder.name}
+                  </p>
+
+                </div>
+              ))}
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+export default Folder;
+
+/*
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AddFolderButton from './AddFolderButton';
@@ -10,33 +68,49 @@ function Folder() {
   const [currentUser, setCurrentUser] = useState(null);
 
   // Fetch parent folders
-  useEffect(() => {
-    (async () => {
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const { data } = await axios.get('/profile');
+  //       setCurrentUser(data);
+  //       const res = await axios.get(`/folder/user/${data?.id}`);
+  //       const foldersData = res.data.folder;
+  //       const parentFoldersData = foldersData.filter(folder => folder.parentId === null);
+  //       setParentFolders(parentFoldersData);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   })();
+  // }, []);
+
+    useEffect(() => {
+    const fetchCurrentUser = async () => {
       try {
         const { data } = await axios.get('/profile');
-        setCurrentUser(data);
-        const res = await axios.get(`/folder/user/${data?.id}`);
-        const foldersData = res.data.folder;
-        const parentFoldersData = foldersData.filter(folder => folder.parentId === null);
-        setParentFolders(parentFoldersData);
+        const folderIdResponse = await axios.get(`/folderId/${data.id}`);
+        const { folderId } = folderIdResponse.data;
+        setCurrentUser({ ...data, folderId });
       } catch (error) {
         console.log(error);
       }
-    })();
+    };
+
+    fetchCurrentUser();
   }, []);
 
-  const handleParentFolderClick = async (parentFolder) => {
-    const res = await axios.get(`/folder/${parentFolder.id}/children`);
-    const childrenFoldersData = res.data.children;
-    setParentFolders(prevFolders => {
-      const updatedFolders = [...prevFolders];
-      const folderIndex = updatedFolders.findIndex(folder => folder.id === parentFolder.id);
-      if (folderIndex !== -1) {
-        updatedFolders[folderIndex].children = childrenFoldersData;
-      }
-      return updatedFolders;
-    });
-  };
+
+  // const handleParentFolderClick = async (parentFolder) => {
+  //   const res = await axios.get(`/folder/${parentFolder.id}/children`);
+  //   const childrenFoldersData = res.data.children;
+  //   setParentFolders(prevFolders => {
+  //     const updatedFolders = [...prevFolders];
+  //     const folderIndex = updatedFolders.findIndex(folder => folder.id === parentFolder.id);
+  //     if (folderIndex !== -1) {
+  //       updatedFolders[folderIndex].children = childrenFoldersData;
+  //     }
+  //     return updatedFolders;
+  //   });
+  // };
 
   return (
     <>
@@ -71,4 +145,4 @@ function Folder() {
 }
 
 export default Folder;
-
+*/
