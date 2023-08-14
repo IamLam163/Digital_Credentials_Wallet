3eebe077446f3cb6a997533ef50d2dc023ec4c0e
erickadikah <erickadikah2030@gmail.com>
2023-08-14 09:25:04 +0300

    update url back-end

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -17,7 +17,7 @@ const router = express.Router();
 router.use(
   cors({
     credentials: true,
-    origin: "https://digital-wallet.onrender.com || http://localhost:3000",
+    origin: "https://digital-wallet.onrender.com " || "http://localhost:7000",
     // origin: 'http://localhost:3000' || 'https://digital-credentials-wallet-git-latest-iamlam163.vercel.app'
   }),
 );
